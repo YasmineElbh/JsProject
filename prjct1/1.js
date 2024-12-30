b3dc714167b0cf8ed9247851d16cad18612e24c4
YasmineElbh <elbahiyasmine.4@gmail.com>
2024-12-30 23:54:35 +0100

    update

--- prjct1/1.js
+++ prjct1/1.js
@@ -1,3 +1,12 @@
 document.querySelector('#push').onclick = function(){
-    if(document.querySelector('#newatask input').)
+    if(document.querySelector('#newatask input').value.length == 0){
+        alert("Please Enter a Task");
+    }
+    else{
+        document.querySelector('#tasks').innerHTML += 
+        `<div class="task">
+            <span id ="takename"> ${document.querySelector('#newatask input').value}</span>
+            <button class="delete"><i class="fa-solid fa-delete-left"></i></button>
+        </div>`;
+    }
 }
